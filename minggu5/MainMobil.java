40c416748183a1f0a7607b592171ea9c0819421e
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2024-03-24 13:50:20 +0700

    Latihan

---
+++ minggu5/MainMobil.java
@@ -0,0 +1,14 @@
+package minggu5;
+
+public class MainMobil {
+    public static void main(String[] args) {
+        Mobil output = new Mobil();
+
+        System.out.println("=======================================");
+        System.out.println("Mobil dengan top_acceleration tertinggi: " + output.cariMax(output.top_acceleration, 0, output.top_acceleration.length - 1));
+        System.out.println("=======================================");
+        System.out.println("Mobil dengan top_acceleration terendah: " + output.cariMin(output.top_acceleration, 0, output.top_acceleration.length - 1));
+        System.out.println("=======================================");
+        System.out.println("Rata-rata top_speed dari seluruh mobil: " + output.power(output.top_power));
+    }
+}
---
+++ minggu5/Mobil.java
@@ -0,0 +1,40 @@
+package minggu5;
+
+public class Mobil {
+    String[] merk = {"BMW", "Ford", "Nissan", "Subaru", "Subaru", "Toyota", "Toyota", "Volkswagen"};
+    String[] tipe = {"M2 Copue", "Fiesta ST", "370Z", "BRZ", "Impreza WRX STI", "AE86 Trueno", "86/GT86", "Golf GTI"};
+    int[] tahun = {2016, 2014, 2009, 2014, 2013, 1986, 2014, 2014};
+    int[] top_acceleration = {6816, 3921, 4360, 4058, 6255, 3700, 4180, 4180};
+    int[] top_power = {728, 575, 657, 609, 703, 553, 609, 631};
+
+    int cariMax(int[] arr, int kiri, int kanan){
+        if (kiri == kanan) {
+            return arr[kiri];
+        }
+        
+        int mid = kiri + (kanan - kiri) / 2;
+        int maxKiri = cariMax(arr, kiri, mid);
+        int maxKanan = cariMax(arr, mid + 1, kanan);
+
+        return Math.max(maxKiri, maxKanan);
+    }
+
+    int cariMin(int[] arr, int kiri, int kanan){
+        if (kiri == kanan) {
+            return arr[kiri];
+        }
+        int mid = kiri + (kanan - kiri) / 2;
+        int minKiri = cariMin(arr, kiri, mid);
+        int minKanan = cariMin(arr, mid + 1, kanan);
+
+        return Math.min(minKiri, minKanan);
+    }
+
+    double power(int[] arr){
+        double sum = 0;
+        for (int num : arr) {
+            sum += num;
+        }
+        return sum / arr.length;
+    }
+}
