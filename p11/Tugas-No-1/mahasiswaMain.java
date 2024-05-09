800e005da06f58ed3b290e35ea0928d2892a901d
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2024-05-09 12:32:53 +0700

    Tugas no 1

---
+++ p11/Tugas-No-1/mahasiswaMain.java
@@ -0,0 +1,12 @@
+public class mahasiswaMain {
+    public static void main(String[] args) {
+        SingleLinkList sll = new SingleLinkList();
+
+        sll.addFirst(111, "Anton");
+        sll.addLast(112, "Prita");
+        sll.addLast(113, "Yusuf");
+        sll.addLast(114, "Doni");
+        sll.addLast(115, "Sari");
+        sll.print();
+    }
+}
---
+++ p11/Tugas-No-1/node.java
@@ -0,0 +1,11 @@
+public class node {
+    int nim;
+    String nama;
+    node next;
+
+    node (int nim, String nama, node berikutnya){
+        this.nim = nim;
+        this.nama = nama;
+        next = berikutnya;
+    }
+}
